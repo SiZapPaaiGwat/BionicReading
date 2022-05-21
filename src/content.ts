6c669db5e2e5d1a5daca7a0b91349584e5d1bf89
世界之极 <xugaofan@gmail.com>
2022-05-21 21:35:25 +0800

    Fisrt blood

---
+++ src/content.ts
@@ -0,0 +1,155 @@
+const EXT_NAME = "Bionic Reading";
+const MIN_WORDS = 4;
+const MAX_BOLD_LETTERS = 6;
+const EN_WORD_REG = /[a-z][a-z0-9]+/gi;
+const INLINE_DECORATOR = "a,em,strong,time".split(",");
+const IGNORED_PARENT_TAGS =
+  "script,style,pre,code,iframe,select,input,button,textarea,form,svg".split(
+    ","
+  );
+
+enum Tag {
+  Word = "bionic-word",
+  Font = "bionic-font",
+}
+
+/**
+ * Returns a function, that, as long as it continues to be invoked, will not
+ * be triggered. The function will be called after it stops being called for
+ * N milliseconds. If `immediate` is passed, trigger the function on the
+ * leading edge, instead of the trailing. The function also has a property 'clear'
+ * that is a function which will clear the timer to prevent previously scheduled executions.
+ *
+ * @source underscore.js
+ * @see http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
+ * @param {Function} function to wrap
+ * @param {Number} timeout in ms (`100`)
+ * @param {Boolean} whether to execute at the beginning (`false`)
+ * @api public
+ */
+function debounce(func, wait, immediate) {
+  var timeout, args, context, timestamp, result;
+  if (null == wait) wait = 100;
+
+  function later() {
+    var last = Date.now() - timestamp;
+
+    if (last < wait && last >= 0) {
+      timeout = setTimeout(later, wait - last);
+    } else {
+      timeout = null;
+      if (!immediate) {
+        result = func.apply(context, args);
+        context = args = null;
+      }
+    }
+  }
+
+  var debounced = function () {
+    context = this;
+    args = arguments;
+    timestamp = Date.now();
+    var callNow = immediate && !timeout;
+    if (!timeout) timeout = setTimeout(later, wait);
+    if (callNow) {
+      result = func.apply(context, args);
+      context = args = null;
+    }
+
+    return result;
+  };
+
+  debounced.clear = function () {
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = null;
+    }
+  };
+
+  debounced.flush = function () {
+    if (timeout) {
+      result = func.apply(context, args);
+      context = args = null;
+
+      clearTimeout(timeout);
+      timeout = null;
+    }
+  };
+
+  return debounced;
+}
+
+function acceptNode(node: Node) {
+  const parent = node.parentElement;
+  const tag = parent.tagName.toLowerCase();
+  if (
+    tag === Tag.Word ||
+    tag === Tag.Font ||
+    IGNORED_PARENT_TAGS.includes(tag)
+  ) {
+    return NodeFilter.FILTER_REJECT;
+  }
+
+  if (
+    !INLINE_DECORATOR.includes(tag) &&
+    parent.textContent.split(/\s/).length < MIN_WORDS
+  ) {
+    return NodeFilter.FILTER_REJECT;
+  }
+
+  const { top, bottom } = parent.getBoundingClientRect();
+  const h = document.documentElement.clientHeight;
+  if (bottom < -100 || top > h + 100) {
+    return NodeFilter.FILTER_REJECT;
+  }
+
+  return NodeFilter.FILTER_ACCEPT;
+}
+
+function bionic(node: Text) {
+  const text = node.data;
+  const el = document.createElement(Tag.Word);
+  el.innerHTML = text.replace(EN_WORD_REG, (word) => {
+    const midIndex = Math.min(MAX_BOLD_LETTERS, Math.ceil(word.length / 2));
+    return `<${Tag.Font}>${word.slice(0, midIndex)}</${Tag.Font}>${word.slice(
+      midIndex
+    )}`;
+  });
+  node.after(el);
+  node.remove();
+}
+
+function iter() {
+  console.time(`[${EXT_NAME}]`);
+  const treeWalker = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_TEXT,
+    {
+      acceptNode,
+    }
+  );
+  const list = [];
+  while (treeWalker.nextNode()) {
+    const node = treeWalker.currentNode;
+    list.push(node);
+  }
+  list.forEach(bionic);
+  console.timeEnd(`[${EXT_NAME}]`);
+}
+
+const styleEl = document.createElement("style");
+styleEl.innerHTML = `
+  ${Tag.Word} > ${Tag.Font} {
+    font-weight:bold;
+  }
+`;
+document.head.appendChild(styleEl);
+const main = debounce(iter, 180);
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  document.addEventListener("scroll", main);
+  document.addEventListener("wheel", main);
+  window.addEventListener("resize", main);
+  main();
+  sendResponse({ message: "DONE" });
+});
